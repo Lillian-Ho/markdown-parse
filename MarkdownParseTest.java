af24356d9a3d33c436be1785b3cd2bd00ac0627d
LillianHo5 <lmho@ucsd.edu>
2022-01-26 16:55:00 -0800

    Additional JUnit Tests with other markdown files

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,46 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.*;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test 
+    public void firstTest() throws IOException { 
+        Path fileName = Path.of("test-file.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of("https://something.com", "some-page.html"), links);
+    }
+
+    @Test 
+    public void secondTest() throws IOException { 
+        Path fileName = Path.of("new-file.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of("https://ucsd-cse15l-w22.github.io/week/week3/"), links);
+    }
+
+    @Test 
+    public void thirdTest() throws IOException { 
+        Path fileName = Path.of("no-paren.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of(), links);
+    }
+
+    @Test 
+    public void fourthTest() throws IOException { 
+        Path fileName = Path.of("first-line.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of("https://hello.com"), links);
     }
 }
